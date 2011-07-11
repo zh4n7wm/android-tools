a1936600f2a6d17978fdce3481974b97bce86cf9
ox0spy <ox0spy@gmail.com>
2011-07-11 17:58:16 +0800

    repack-bootimg.sh: add some Android makefile's sentence, easily find next time

--- repack-bootimg.sh
+++ repack-bootimg.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # ox0spy <ox0spy@gmail.com>
 # 杀鸡焉用牛刀 ... bash简单明了
+#
+# Android系统boot.img生成过程:
+#   core/Makefile:$(INSTALLED_BOOTIMAGE_TARGET): $(MKEXT2IMG) $(INTERNAL_BOOTIMAGE_FILES)
+#   core/config.mk:MKEXT2IMG := $(HOST_OUT_EXECUTABLES)/genext2fs$(HOST_EXECUTABLE_SUFFIX)
 
 usage="$0 <kernel> <ramdisk-directory> <outfile>"
 
@@ -12,14 +16,14 @@ else
 	outfile="$3"
 fi
 
-cmdline='console=ttyMSM1,115200n8 androidboot.hardware=qcom'
-#cmdline='no_console_suspend=1 console=null'
+cmdline="console=ttyMSM1,115200n8 androidboot.hardware=qcom"
+#cmdline="no_console_suspend=1 console=null"
 
 cur_dir=$(pwd)
 ramdisk_cpio_gz=$(mktemp)
 
 cd $ramdisk_dir &&
-	$(find . | cpio -o -H newc | gzip > $ramdisk_cpio_gz);
+	$(find . | cpio -o -H newc | gzip > $ramdisk_cpio_gz)
 
 cd $cur_dir &&
 	$(mkbootimg --cmdline "$cmdline" \
